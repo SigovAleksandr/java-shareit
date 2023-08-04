7bceb4611428687441ce1ebff46c757d0edc8a44
Aleksandr Sigov <paperrcutt@yandex.ru>
2023-08-04 10:33:49 +0300

    another codestyle fix

--- src/main/java/ru/practicum/shareit/booking/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/BookingRepository.java
@@ -19,7 +19,7 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
     List<Booking> findByBookerIdAndStatusIsOrderByStartDesc(long userId, BookingStatus status);
 
     List<Booking> findByItemIdOrderByStartDesc(long itemId);
-    
+
     List<Booking> findByItemIdInAndEndIsBeforeOrderByEndDesc(List<Long> ids, LocalDateTime time);
 
     List<Booking> findByItemIdInAndStartIsAfterOrderByEndDesc(List<Long> itemIds, LocalDateTime time);
@@ -28,20 +28,10 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     List<Booking> findByItemIdInAndStatusIsOrderByStartDesc(List<Long> itemIds, BookingStatus status);
 
-    @Query("select b " +
-            "from Booking b " +
-            "where b.item.id = ?1 " +
-            "and b.status <> 'REJECTED' " +
-            "and b.end > CURRENT_TIMESTAMP " +
-            "order by b.start asc")
+    @Query("select b " + "from Booking b " + "where b.item.id = ?1 " + "and b.status <> 'REJECTED' " + "and b.end > CURRENT_TIMESTAMP " + "order by b.start asc")
     List<Booking> findNextBooking(long itemId);
 
-    @Query("select b " +
-            "from Booking b " +
-            "where b.item.id = ?1 " +
-            "and b.status <> 'REJECTED' " +
-            "and b.start < current_timestamp " +
-            "order by b.end asc")
+    @Query("select b " + "from Booking b " + "where b.item.id = ?1 " + "and b.status <> 'REJECTED' " + "and b.start < current_timestamp " + "order by b.end asc")
     List<Booking> findLastBooking(long itemId);
 
     List<Booking> findByBookerIdAndItemId(long userId, long itemid);
